89fccab9506d8bb38d580dcc52a2ea60031a23cf
Steven Calverley <steven@stevencalverley.com>
2024-11-15 11:37:50 +1100

    Update Calendar Event Title

--- src/server/googleCalendar.ts
+++ src/server/googleCalendar.ts
@@ -1,13 +1,13 @@
-import "use-server"
-import { clerkClient } from "@clerk/nextjs/server"
-import { google } from "googleapis"
-import { addMinutes, endOfDay, startOfDay } from "date-fns"
+import "use-server";
+import { clerkClient } from "@clerk/nextjs/server";
+import { google } from "googleapis";
+import { addMinutes, endOfDay, startOfDay } from "date-fns";
 
 export async function getCalendarEventTimes(
   clerkUserId: string,
   { start, end }: { start: Date; end: Date }
 ) {
-  const oAuthClient = await getOAuthClient(clerkUserId)
+  const oAuthClient = await getOAuthClient(clerkUserId);
 
   const events = await google.calendar("v3").events.list({
     calendarId: "primary",
@@ -17,27 +17,27 @@ export async function getCalendarEventTimes(
     timeMax: end.toISOString(),
     maxResults: 2500,
     auth: oAuthClient,
-  })
+  });
 
   return (
     events.data.items
-      ?.map(event => {
+      ?.map((event) => {
         if (event.start?.date != null && event.end?.date != null) {
           return {
             start: startOfDay(event.start.date),
             end: endOfDay(event.end.date),
-          }
+          };
         }
 
         if (event.start?.dateTime != null && event.end?.dateTime != null) {
           return {
             start: new Date(event.start.dateTime),
             end: new Date(event.end.dateTime),
-          }
+          };
         }
       })
-      .filter(date => date != null) || []
-  )
+      .filter((date) => date != null) || []
+  );
 }
 
 export async function createCalendarEvent({
@@ -49,18 +49,18 @@ export async function createCalendarEvent({
   durationInMinutes,
   eventName,
 }: {
-  clerkUserId: string
-  guestName: string
-  guestEmail: string
-  startTime: Date
-  guestNotes?: string | null
-  durationInMinutes: number
-  eventName: string
+  clerkUserId: string;
+  guestName: string;
+  guestEmail: string;
+  startTime: Date;
+  guestNotes?: string | null;
+  durationInMinutes: number;
+  eventName: string;
 }) {
-  const oAuthClient = await getOAuthClient(clerkUserId)
-  const calendarUser = await clerkClient().users.getUser(clerkUserId)
+  const oAuthClient = await getOAuthClient(clerkUserId);
+  const calendarUser = await clerkClient().users.getUser(clerkUserId);
   if (calendarUser.primaryEmailAddress == null) {
-    throw new Error("Clerk user has no email")
+    throw new Error("Clerk user has no email");
   }
 
   const calendarEvent = await google.calendar("v3").events.insert({
@@ -83,30 +83,30 @@ export async function createCalendarEvent({
       end: {
         dateTime: addMinutes(startTime, durationInMinutes).toISOString(),
       },
-      summary: `${guestName} + ${calendarUser.fullName}: ${eventName}`,
+      summary: `[${eventName}] ${guestName} + ${calendarUser.fullName}`,
     },
-  })
+  });
 
-  return calendarEvent.data
+  return calendarEvent.data;
 }
 
 async function getOAuthClient(clerkUserId: string) {
   const token = await clerkClient().users.getUserOauthAccessToken(
     clerkUserId,
     "oauth_google"
-  )
+  );
 
   if (token.data.length === 0 || token.data[0].token == null) {
-    return
+    return;
   }
 
   const client = new google.auth.OAuth2(
     process.env.GOOGLE_OAUTH_CLIENT_ID,
     process.env.GOOGLE_OAUTH_CLIENT_SECRET,
     process.env.GOOGLE_OAUTH_REDIRECT_URL
-  )
+  );
 
-  client.setCredentials({ access_token: token.data[0].token })
+  client.setCredentials({ access_token: token.data[0].token });
 
-  return client
+  return client;
 }
